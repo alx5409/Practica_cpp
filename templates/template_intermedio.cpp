c31702372af86b791014934cd7c962ac82e85634
Alex <alx5409@gmail.com>
2025-10-10 17:54:35 +0200

    Ejercicio 2 de template_intermedio hecho

--- templates/template_intermedio.cpp
+++ templates/template_intermedio.cpp
@@ -5,6 +5,14 @@
 #include <map>
 #include <set>
 
+template <typename T>
+void mostrar_vector(std::vector<T> vector) {
+    for (int i = 0; i < vector.size(); i++) {
+        std::cout << vector[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
 // Ejercicio 1: Escribe una función template que reciba dos vectores y devuelva un vector con la unión de ambos (sin duplicados).
 template <typename T>
 std::vector<T> unir_vectores(std::vector<T> vector_1, std::vector<T> vector_2) {
@@ -26,13 +34,26 @@ void main_1() {
     std::vector<int> vector_1 = {1, 2, 3, 4};
     std::vector<int> vector_2 = {4, 6, 2, 9};
     std::vector<int> vector_union = unir_vectores(vector_1, vector_2);
-    for (int i = 0; i < vector_union.size(); i++) {
-        std::cout << vector_union[i] << " ";
-    }
-    std::cout << std::endl;
+    mostrar_vector(vector_union);
 }
 
 // Ejercicio 2: Escribe una función template que reciba un vector y un valor, y elimine todas las apariciones de ese valor del vector.
+template <typename T>
+void eliminar_duplicados(std::vector<T>& vector, T valor) {
+    int i = 0;
+    vector.erase(
+        std::remove(vector.begin(), vector.end(), valor),
+        vector.end()
+    );
+}
+
+void main_2() {
+    std::vector<int> vector = { 1, 2, 3, 2, 1, 5, 6};
+    int valor = 2;
+    mostrar_vector(vector);
+    eliminar_duplicados(vector, valor);
+    mostrar_vector(vector);
+}
 
 // Ejercicio 3: Escribe una función template que reciba un vector y devuelva el elemento más frecuente.
 
@@ -61,6 +82,6 @@ void main_1() {
 // Ejercicio 15: Escribe una función template que reciba un vector y devuelva un mapa con la frecuencia de cada elemento.
 
 int main() {
-    main_1();
+    main_2();
     return 0;
 }
