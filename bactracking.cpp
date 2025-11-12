c9a5024791509c9f96498e71fcd0ea1950878140
Alex <alx5409@gmail.com>
2025-11-12 14:34:59 +0100

    Retry exercise 8

--- bactracking.cpp
+++ bactracking.cpp
@@ -644,42 +644,33 @@ void main_7() {
 }
 
 // 8. Partición de un conjunto en k subconjuntos con suma igual
-bool suman_igual(const std::vector<int> &conjunto1, const std::vector<int> &conjunto2, int k) {
-    int suma1 = 0;
-    int suma2 = 0;
-    for (int num : conjunto1) {
-        suma1 += num;
+//    Dado un conjunto de números, determina si se puede dividir en k subconjuntos con la misma suma.
+bool suman_igual(const std::vector<std::vector<int>> &subconjuntos, int suma k) {
+    for (const auto &subconjunto : subconjuntos) {
+        int suma_subconjunto = 0;
+        for (int num : subconjunto) {
+            suma_subconjunto += num;
+        }
+        if (suma_subconjunto != suma) {
+            return false;
+        }
     }
-    for (int num : conjunto2) {
-        suma2 += num;
-    }
-    return suma1 == suma2;
+    return true;
 }
 
-std::vector<std::vector<int>> subconjuntos_long_k(const std::vector<int> &numeros, int k) {
-
-    return {};
-}
-
-std::vector<std::vector<int>> particionar_en_k_subconjuntos_con_suma_igual(const std::vector<int> &numeros, int k) {
-    return {};
-}
-
-bool existe_particion_k_subconjuntos(const std::vector<int> &numeros, int k) {
-    return false;
+bool existe_particion_k_subconjuntos(const std::vector<int> &conjunto, int k) {
+    
 }
 
 void main_8() {
-    std::vector<int> numeros = {4, 3, 2, 3, 5, 2, 1};
+    std::vector<int> conjunto = {4, 3, 2, 3, 5, 2, 1};
     int k = 4;
-    if (existe_particion_k_subconjuntos(numeros, k)) {
+    if (existe_particion_k_subconjuntos(conjunto, k)) {
         std::cout << "Es posible particionar el conjunto en " << k << " subconjuntos con la misma suma.\n";
-    } else {
-        std::cout << "No es posible particionar el conjunto en " << k << " subconjuntos con la misma suma.\n";
+        return;
     }
+    std::cout << "No es posible particionar el conjunto en " << k << " subconjuntos con la misma suma.\n";
 }
-
-//    Dado un conjunto de números, determina si se puede dividir en k subconjuntos con la misma suma.
 
 // 9. Colorear un grafo (Graph Coloring)
 //    Dado un grafo y un número de colores, asigna un color a cada nodo de modo que no haya dos nodos adyacentes con el mismo color.
